c1c4b727e8368bfc86742b55576b4528e1e07299
Mario Batres <mario.batres.gt@gmail.com>
2020-09-16 20:26:10 -0600

    Agregand query 3.

--- jdbc-basico/src/main/java/edu/academik/telus/jdbc/basico/Main.java
+++ jdbc-basico/src/main/java/edu/academik/telus/jdbc/basico/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -60,6 +61,38 @@ public class Main {
         }
     }
 
+    private static void ejecutarQuery3(Connection conn, Integer clienteId) throws SQLException {
+
+        String queryString = "select \n"
+                + "	c.cliente_id,\n"
+                + "	c.nombre cliente_nombre,\n"
+                + "	v.venta_id, \n"
+                + "	v.fecha_factura,\n"
+                + "	v.numero \n"
+                + "from venta v \n"
+                + "inner join cliente c on c.cliente_id = v.cliente_id\n"
+                + "where c.cliente_id = ?";
+
+        // try with resource ---> el va  a cerrar el stmt
+        try (PreparedStatement stmt = conn.prepareStatement(queryString)) {
+
+            stmt.setInt(1, clienteId);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+
+                while (rs.next()) {
+                    //int clienteId = rs.getInt("cliente_id");
+                    String clienteNombre = rs.getString("cliente_nombre");
+                    int ventaId = rs.getInt("venta_id");
+                    Timestamp fechaFactura = rs.getTimestamp("fecha_factura");
+                    String numero = rs.getString("numero");
+
+                    System.out.println(clienteNombre + "\t" + ventaId + "\t" + fechaFactura + "\t" + numero);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         Connection conn = null;
@@ -70,8 +103,9 @@ public class Main {
 
             System.out.println("Base de datos conectada!!!");
 
-            //ejecutarQuery1(conn);
+            ejecutarQuery1(conn);
             ejecutarQuery2(conn);
+            ejecutarQuery3(conn, 81);
 
             conn.close();
 
